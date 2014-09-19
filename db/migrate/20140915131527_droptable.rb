99fb5dfea1ab14f218de3b480e9bc69074b57e0b
Krishna vyas <Krishnavyas.vyas373@gmail.com>
2014-09-19 13:10:49 +0530

    Delete Migration Files

--- db/migrate/20140915131527_droptable.rb
+++
@@ -1,6 +0,0 @@
-class Droptable < ActiveRecord::Migration
-  def change
-  	drop_table :category_products
-  	drop_table :product_categories
-  end
-end
--- db/migrate/20140916120918_remove_colum.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColum < ActiveRecord::Migration
-  def self.up
-  remove_column :products, :f_product
-	end
-end
